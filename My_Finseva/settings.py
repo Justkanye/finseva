4e3fd264201b6e040eea12b6057b90c8b3a650fc
Justkanye <adekanyetimilehin2000@gmail.com>
2022-03-09 17:45:50 +0100

    mysql

--- My_Finseva/settings.py
+++ My_Finseva/settings.py
@@ -87,13 +87,24 @@ WSGI_APPLICATION = 'My_Finseva.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'dbs5662271',
+        'USER': 'dbu2928818',
+        'PASSWORD': 'Lkjh@789456',
+        'HOST': 'db5006858383.hosting-data.io',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
